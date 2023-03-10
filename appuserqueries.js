fb8bdcbc92b45e40c247a383c21e5d32a4a9caf1
om siddhapura <omsiddhapura1504@gmail.com>
2023-03-10 09:08:18 +0530

    commit

--- appuserqueries.js
+++ appuserqueries.js
@@ -25,24 +25,30 @@ const createUser = (req, res) => {
 
 
 const loginUser = (req, res) => {
-    const { uname, upassword } = req.body
+    const { uname, upassword } = req.query
+
     const checkcrendtials = 'select * from appuser where uname = $1 and upassword = $2';
     // console.log(insertUser);
     client.query(checkcrendtials, [uname, upassword], (err, response) => {
         if (!err) {
             // console.log('data is there...');
             // response.status(201).send()
-            console.log(res.rows)
-            if (res.rowCount == 1) {
+            
+            if (response.rowCount == 1) {
                 // console.log("user found!!");
                 // return response.status(201).send()    
                 res.setHeader('content-type', 'application/json');
-                return res.status(200).json({ "uname": uname, "upassword": upassword });
+                return res.status(200).json({message:'success'});
             }
-
+            else{
+                res.setHeader('content-type', 'application/json');
+                return res.status(200).json({ message:'invalidecredentials'});
+            }
         }
         else {
             console.log(err.message);
+            res.setHeader('content-type', 'application/json');
+            return res.status(200).json({message:'error'});
         }
         client.end;
     })
--- taskqueries.js
+++ taskqueries.js
@@ -24,9 +24,10 @@ const createTask = (req, res) => {
 }
 
 const deleteTask = (req, res) => {
-    const { taskid, userid } = req.body
+    const { taskid, userid } = req.query
     // DELETE FROM table_name
     // WHERE[condition];
+    // console.log(req);
     const deleteTask = 'delete from usertasks where taskid = $1 and userid = $2';
     console.log(deleteTask);
     client.query(deleteTask, [taskid, userid], (err, response) => {
@@ -35,25 +36,26 @@ const deleteTask = (req, res) => {
             // response.status(201).send()
             console.log("task data deleted successfully");
             res.setHeader('content-type', 'application/json');
-            return res.status(200).json({ msg: "Success" });
+            return res.status(200).json({ msg: "success" });
         }
         else {
             console.log(err);
             res.setHeader('content-type', 'application/json');
-            return res.status(200).json({ msg: "Success" });
+            return res.status(200).json({ msg: "error" });
         }
         client.end;
     })
 }
 
 const editTask = (req, res) => {
-    const { taskid, title, description, dateadded, duedate, iscompleted, isimportant, userid  } = req.body
+    const { taskid, title, description, dateadded, duedate, isCompleted, isImportant, userid  } = req.body
     
     const editTask = 'insert into usertasks (taskid, title, description, dateadded, duedate ,iscompleted,isimportant,userid) values ($1, $2, $3, $4, $5,$6,$7,$8) on conflict (taskid) do update set title = excluded.title , description = excluded.description , duedate = excluded.duedate , iscompleted = excluded.iscompleted , isimportant = excluded.isimportant';
-    client.query(editTask, [taskid, title, description, dateadded, duedate, iscompleted, isimportant, userid ], (err, response) => {
+    client.query(editTask, [taskid, title, description, dateadded, duedate, isCompleted, isImportant, userid ], (err, response) => {
         if (!err) {
             // console.log('data is there...');
             // response.status(201).send()
+            // console.log();
             console.log("task data edited successfully");
             res.setHeader('content-type', 'application/json');
             return res.status(200).json({ msg: "Success" });
@@ -68,17 +70,26 @@ const editTask = (req, res) => {
 }
 
 const getTasks = (req, res) => {
-    const { userid  } = req.body
+    let { userid  } = req.query
+    console.log('====================================');
+    console.log(req.query);
+    console.log('====================================');
     // DELETE FROM table_name
     // WHERE[condition];
+    userid=Number(userid)
+    console.log('====================================');
+    console.log(typeof userid);
+    console.log('====================================');
     const getTaskQuery = 'select * from usertasks where userid = $1'
-    // console.log(deleteTask);
+    console.log(getTaskQuery);
     client.query(getTaskQuery, [userid ], (err, response) => {
         if (!err) {
             // console.log('data is there...');
-            
+            console.log('====================================');
+            console.log(response);
+            console.log('====================================');
             res.setHeader('content-type', 'application/json');
-            return res.status(200).json({data: res.rows});
+            return res.status(200).json({data: response.rows});
         }
         else {
             console.log(err);
